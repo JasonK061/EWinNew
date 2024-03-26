6754683a13282511b968dc208d274fcda6ab7b67
jisdom <jisdom2020@gmail.com>
2024-03-26 13:58:05 +0800

    收藏頁調整

--- src/view/game_favorite/index.js
+++ src/view/game_favorite/index.js
@@ -147,94 +147,100 @@ function Gamefavorite(props) {
             {
                 isLoading ? (<Loading />) : (
                     <div className="section_box" style={{ width: '100%' }}>
-                        <ul>
-                            {tiList && tiList.TableInfoList && tiList.TableInfoList.map((i, index) => {
-                                if (Favos.includes(i.TableNumber)) {
-                                    return (
-                                        <li key={index}
-                                            onMouseEnter={() => setHoveredItem(i.TableNumber)}
-                                            onMouseLeave={mouseleave}
-                                            className='li-box'
-                                        >
-                                            <span className={`${Favos.includes(i.TableNumber) ? 'has-favorites' : ''}`} />
-                                            <div className={`games ${i.TableNumber}`}>
-                                                {/* 獲取ImageType為1的ImageUrl */}
-                                                {i.ImageList && i.ImageList.find(image => image.ImageType === 1) && (
-                                                    <img src={i.ImageList.find(image => image.ImageType === 1).ImageUrl} alt="Table Image" />
-                                                )}
-                                                <RoadMap shoeResults={shoeResults} />
-                                            </div>
-                                            <p className='game-title'>
-                                                {i.TableNumber}
-                                            </p>
-                                            <p className='game-wallet'>
-                                                <span>{userInfo.BetLimitCurrencyType}</span>
-                                                <span>
-                                                    {userInfo && userInfo.Wallet && userInfo.Wallet.map((i, index) => (
-                                                        i.CurrencyType === userInfo.BetLimitCurrencyType ? <span className='without-mr' key={index}>{Math.floor(i.Balance)}</span> : ''
-                                                    ))}
-                                                </span>
-                                            </p>
-
-                                            <div className={`hover-box ${hoveredItem === i.TableNumber ? 'visible' : ''} ${moreScale}`}>
-                                                <span className='close-hover-box' onClick={() => { setHoveredItem(null) }}></span>
+                        {Favos.length === 0 ? (
+                            <div className='without_favorite'>
+                                <h2>{t("Global.without_favorite")}</h2>
+                            </div>
+                        ) : (
+                            <ul>
+                                {tiList && tiList.TableInfoList && tiList.TableInfoList.map((i, index) => {
+                                    if (Favos.includes(i.TableNumber)) {
+                                        return (
+                                            <li key={index}
+                                                onMouseEnter={() => setHoveredItem(i.TableNumber)}
+                                                onMouseLeave={mouseleave}
+                                                className='li-box'
+                                            >
+                                                <span className={`${Favos.includes(i.TableNumber) ? 'has-favorites' : ''}`} />
                                                 <div className={`games ${i.TableNumber}`}>
                                                     {/* 獲取ImageType為1的ImageUrl */}
                                                     {i.ImageList && i.ImageList.find(image => image.ImageType === 1) && (
                                                         <img src={i.ImageList.find(image => image.ImageType === 1).ImageUrl} alt="Table Image" />
                                                     )}
+                                                    <RoadMap shoeResults={shoeResults} />
                                                 </div>
-                                                <div className='info-box'>
-                                                    <p className='game-title'>
-                                                        {i.TableNumber}
-                                                    </p>
-                                                    <p className='game-wallet'>
-                                                        <span>{userInfo.BetLimitCurrencyType}</span>
-                                                        <span>
-                                                            {userInfo && userInfo.Wallet && userInfo.Wallet.map((i, index) => (
-                                                                i.CurrencyType === userInfo.BetLimitCurrencyType ? <span className='without-mr' key={index}>{i.Balance}</span> : ''
-                                                            ))}
-                                                        </span>
-                                                    </p>
-                                                    <div className='game-start' >
-                                                        <Link to={`/games/${i.TableNumber}`} onClick={getGameName(i.TableNumber, i.TableTimeoutSecond)}>{t("Global.start_games")}</Link>
-                                                    </div>
-                                                    <div className='game-table-wrap'>
-                                                        <RoadMap shoeResults={shoeResults} />
-                                                    </div>
-                                                    <p className='game-dis'>
-                                                        {i.Status}
-                                                    </p>
+                                                <p className='game-title'>
+                                                    {i.TableNumber}
+                                                </p>
+                                                <p className='game-wallet'>
+                                                    <span>{userInfo.BetLimitCurrencyType}</span>
+                                                    <span>
+                                                        {userInfo && userInfo.Wallet && userInfo.Wallet.map((i, index) => (
+                                                            i.CurrencyType === userInfo.BetLimitCurrencyType ? <span className='without-mr' key={index}>{Math.floor(i.Balance)}</span> : ''
+                                                        ))}
+                                                    </span>
+                                                </p>
 
-                                                    {moreScale === 'more-scale'
-                                                        ?
-                                                        <div className='show-similar-games forpc'>
+                                                <div className={`hover-box ${hoveredItem === i.TableNumber ? 'visible' : ''} ${moreScale}`}>
+                                                    <span className='close-hover-box' onClick={() => { setHoveredItem(null) }}></span>
+                                                    <div className={`games ${i.TableNumber}`}>
+                                                        {/* 獲取ImageType為1的ImageUrl */}
+                                                        {i.ImageList && i.ImageList.find(image => image.ImageType === 1) && (
+                                                            <img src={i.ImageList.find(image => image.ImageType === 1).ImageUrl} alt="Table Image" />
+                                                        )}
+                                                    </div>
+                                                    <div className='info-box'>
+                                                        <p className='game-title'>
+                                                            {i.TableNumber}
+                                                        </p>
+                                                        <p className='game-wallet'>
+                                                            <span>{userInfo.BetLimitCurrencyType}</span>
+                                                            <span>
+                                                                {userInfo && userInfo.Wallet && userInfo.Wallet.map((i, index) => (
+                                                                    i.CurrencyType === userInfo.BetLimitCurrencyType ? <span className='without-mr' key={index}>{i.Balance}</span> : ''
+                                                                ))}
+                                                            </span>
+                                                        </p>
+                                                        <div className='game-start' >
+                                                            <Link to={`/games/${i.TableNumber}`} onClick={getGameName(i.TableNumber, i.TableTimeoutSecond)}>{t("Global.start_games")}</Link>
+                                                        </div>
+                                                        <div className='game-table-wrap'>
+                                                            <RoadMap shoeResults={shoeResults} />
+                                                        </div>
+                                                        <p className='game-dis'>
+                                                            {i.Status}
+                                                        </p>
+
+                                                        {moreScale === 'more-scale'
+                                                            ?
+                                                            <div className='show-similar-games forpc'>
+                                                                <p>{t("Global.similar_ganes")}</p>
+                                                                <SimilarGames />
+                                                            </div>
+                                                            : ''
+                                                        }
+
+                                                        <div className='show-similar-games formb'>
                                                             <p>{t("Global.similar_ganes")}</p>
                                                             <SimilarGames />
                                                         </div>
-                                                        : ''
-                                                    }
+                                                        <div className='favorites-box'>
+                                                            <span onClick={() => toggleMute(i.TableNumber)} className={`video-control ${props.mutes.includes(i.TableNumber) ? 'video-unmute' : 'video-mute'}`} />
+                                                            <span onClick={() => handleClick(i.TableNumber)} className={Favos.includes(i.TableNumber) ? 'remove-to-favorites' : 'add-to-favorites'} />
 
-                                                    <div className='show-similar-games formb'>
-                                                        <p>{t("Global.similar_ganes")}</p>
-                                                        <SimilarGames />
+                                                        </div>
                                                     </div>
-                                                    <div className='favorites-box'>
-                                                        <span onClick={() => toggleMute(i.TableNumber)} className={`video-control ${props.mutes.includes(i.TableNumber) ? 'video-unmute' : 'video-mute'}`} />
-                                                        <span onClick={() => handleClick(i.TableNumber)} className={Favos.includes(i.TableNumber) ? 'remove-to-favorites' : 'add-to-favorites'} />
-
-                                                    </div>
+                                                    <div className='more forpc' onClick={() => { setMoreScale('more-scale') }} />
                                                 </div>
-                                                <div className='more forpc' onClick={() => { setMoreScale('more-scale') }} />
-                                            </div>
-                                        </li>
-                                    );
-                                } else {
-                                    return null;
-                                }
-                            })}
+                                            </li>
+                                        );
+                                    } else {
+                                        return null;
+                                    }
+                                })}
 
-                        </ul>
+                            </ul>
+                        )}
                     </div>
                 )
             }
